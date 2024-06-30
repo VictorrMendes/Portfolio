36378d1d1d20f19c283cf05782a227dec14fcece
VictorrMendes <victo.mendes.souza@gmail.com>
2024-06-29 22:34:47 -0300

    alterações

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//teste
+const downloadButton = document.querySelector('#butao');
+const box = document.querySelector('.box');
+
+downloadButton.addEventListener('mouseover', function baixar(){
+    box.style.display = 'block';
+},
+);
+downloadButton.addEventListener('mouseout', function baixar(){
+    box.style.display = 'none';
+},
+);
