09d23542ccee16204be6ec6ec9b65218170baeca
VictorrMendes <victo.mendes.souza@gmail.com>
2024-07-01 23:25:48 -0300

    lampada

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const downloadButton = document.querySelector('#butao');
 const box = document.querySelector('.box');
 
+const lamp = document.querySelector('#lamp img')
+
 downloadButton.addEventListener('mouseover', function baixar(){
     box.style.display = 'block';
 },);
@@ -8,3 +10,9 @@ downloadButton.addEventListener('mouseout', function baixar(){
     box.style.display = 'none';
 },);
 
+lamp.addEventListener('mouseout', acender);
+
+function acender() {
+    lamp.src ="./imagem/lampOn.jpeg"
+
+}
