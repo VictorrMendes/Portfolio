265bbf776a13fde9c81a577583ba2fa890b4fc00
victorrmendes <victo.mendes.souza@gmail.com>
2026-04-12 17:04:30 -0300

    Implementando analise SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Press_Start_2P, VT323 } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"; 
 import "./globals.css";
 
 const pressStart = Press_Start_2P({ 
@@ -57,6 +58,8 @@ export default function RootLayout({
       <body className={`${pressStart.variable} ${vt323.variable} antialiased min-h-screen relative`}>
         <div className="pointer-events-none fixed inset-0 z-50 h-full w-full bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] bg-[length:100%_4px,3px_100%] opacity-15"></div>
         {children}
+        
+        <Analytics /> 
       </body>
     </html>
   );
