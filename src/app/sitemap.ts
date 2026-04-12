53716057a90c7b7db58cbe1ecff10e6702d47c35
victorrmendes <victo.mendes.souza@gmail.com>
2026-04-12 10:44:18 -0300

    Ajustando serviços

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.vmstorm.com.br/',
+      url: 'https://www.vmstorm.com.br/', 
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
