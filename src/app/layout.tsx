f8dcf59fad35c11a75affe153e03e52bfe8360d7
victorrmendes <victo.mendes.souza@gmail.com>
2026-09-02 18:40:05 -0300

    feat/adicionando Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Press_Start_2P, VT323 } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { MotionConfig } from "framer-motion";
@@ -71,6 +72,14 @@ export default function RootLayout({
         </MotionConfig>
 
         <Analytics />
+
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=npm";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "yc3w7u40o9");`}
+        </Script>
       </body>
     </html>
   );
